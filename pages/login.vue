af812e3b3c11394e530ac821e7d1926344ce4fed
TomtomPudding <tomtomhobby1125@gmail.com>
2020-07-14 02:41:51 +0900

    [add] login page

---
+++ pages/login.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-row type="flex" justify="space-around" align="middle" style="height: 100vh">
+    <a-col :span="6">
+      <LoginForm />
+    </a-col>
+  </a-row>
+</template>
+
+<script type="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import LoginForm from '~/components/LoginForm.vue'
+@Component({
+  components: {
+    LoginForm
+  }
+})
+export default class login extends Vue { }
+</script>
